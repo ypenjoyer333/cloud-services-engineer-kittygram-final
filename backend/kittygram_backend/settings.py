fff8ddb09aa5c03e5c91336bfe7ffafe6a23d4b3
estronnom <estronnom@gmail.com>
2026-01-21 12:32:24 +0300

    code review fixes - use gunicorn - resolve SECRET_KEY & DEBUG from env - fix admin page

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,15 +1,16 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$"
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
-DEBUG = True
+# defaults to False
+DEBUG = os.getenv("DEBUG", "").lower() in ["t", "true", "1"]
 
 ALLOWED_HOSTS = ["*"]
-
-STATIC_ROOT = "/app/collected_static"
 
 INSTALLED_APPS = [
     "django.contrib.admin",
